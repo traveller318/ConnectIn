327cfd2ef255d46ecfdb8c3d8eb7cce4c64f0005
harshav318 <harshavshah3185@gmail.com>
2024-10-29 19:28:04 +0530

    frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className='m-10'>
+      <Button>hello</Button>
+    </div>
+  )
+}
+
+export default App
