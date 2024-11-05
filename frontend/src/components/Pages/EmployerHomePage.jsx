1688c8105e42258ecd51cfa823c17fcc7f6cc0e0
harshav318 <harshavshah3185@gmail.com>
2024-11-05 10:49:28 +0530

    final commit done!!

--- frontend/src/components/Pages/EmployerHomePage.jsx
+++ frontend/src/components/Pages/EmployerHomePage.jsx
@@ -378,7 +378,7 @@ export default function EmployerHomePage() {
         <h1 className="text-2xl font-semibold mb-6">Posted Jobs</h1>
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
           {jobs.map((job) => (
-            <Card key={job.job_id || index} className="flex flex-col">
+            <Card key={job.job_id} className="flex flex-col">
               <CardHeader>
                 <CardTitle>{job.job_title}</CardTitle>
               </CardHeader>
