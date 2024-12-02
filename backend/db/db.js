1fa17c99629e2ea627e084d4f44928405fce7331
harshav318 <harshavshah3185@gmail.com>
2024-12-02 22:34:17 +0530

    env setting

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2'
 
 const con = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Vaishali@0408',
-    database: 'connectIn'
+    host: "ENTER THE HOST NAME",
+    user: "ENTER THE USER NAME",
+    password: "ENTER THE PASSWORD",
+    database: "ENTER THE DATABASE NAME"
 }).promise();
 
 
