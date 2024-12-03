da738f24643eea380dd84a012f1845df7936099b
harshav318 <harshavshah3185@gmail.com>
2024-12-04 02:27:44 +0530

    some final finishes

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
-
+import userReducer from "./user/user.js"
 export default configureStore({
-  reducer: {}
+  reducer: {
+    user: userReducer
+  }
 })
